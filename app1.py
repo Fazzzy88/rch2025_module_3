8880fcd42de70288d64f591d0835907026db618c
Artem <ahelp1@bk.ru>
2025-03-27 13:12:49 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,29 +1,19 @@
-from flask import Flask,request
-from flask import Response
-
-import os
+from flask import Flask, request, Response
 import socket
-import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = """
-Server hostname <b>{hostname}</b> {serveraddr}<br/>
-Using IP <b>{remoteip}</b><br/>
-URL is {url}
+    return f"""
+Server hostname <b>{socket.gethostname()}</b> {request.host}<br/>
+Using IP <b>{request.remote_addr}</b><br/>
+URL is {request.url}
 """
-    return html.format(hostname=socket.gethostname(),remoteip=request.remote_addr, serveraddr=request.host, url=request.url)
 
 @app.route("/headers")
 def headers():
-  return Response(str(request.headers),mimetype='text/plain')
-  
+    return Response(str(request.headers), mimetype='text/plain')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-	
-	
-	
-	
-	
